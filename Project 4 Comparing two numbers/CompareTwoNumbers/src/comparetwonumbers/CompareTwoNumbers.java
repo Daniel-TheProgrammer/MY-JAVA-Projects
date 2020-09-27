8724eb7aa43cf6305eaea6b87f2e2a34572efa09
Nji Daniel <60320246+Daniel-TheProgrammer@users.noreply.github.com>
2020-09-27 03:02:31 +0100

    Add files via upload

---
+++ Project 4 Comparing two numbers/CompareTwoNumbers/src/comparetwonumbers/CompareTwoNumbers.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package comparetwonumbers;
+
+/**
+ *
+ * @author Bluelime Learning So
+ */
+public class CompareTwoNumbers {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int a=29, b=28;
+        if (a==b) {
+        System.out.println("Both are equal");            
+        }
+        else if (a>b){
+        System.out.println("a is greater than b");
+        }
+        else {
+        System.out.println("b is greater than a"); 
+        }
+        
+    }
+    
+}
